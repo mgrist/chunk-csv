710ba636f21d59675aa03c88e8c46e4af89ff466
mgrist <mgrist@angelo.edu>
2022-11-20 15:56:51 -0600

    new splitString function, allows async

--- index.js
+++ index.js
@@ -2,7 +2,14 @@
 const assert = require("assert");
 const byline = require("byline");
 
-function split(inputStream, opts, createOutputStreamCallback) {
+/**
+ *
+ * @param {Readable} inputStream readable csv stream
+ * @param {any} opts delimeter and lineLimit options as an object
+ * @param {function} createOutputStreamCallback callback function to be executed on each chunk.
+ * @returns {Promise<object>} returns object with total chunks and specified options
+ */
+function splitStream(inputStream, opts, createOutputStreamCallback) {
   let outputStream = null;
   let chunkIndex = 0;
   let lineIndex = 0;
@@ -34,31 +41,29 @@ function split(inputStream, opts, createOutputStreamCallback) {
       return;
     }
 
-    lineStream.on("readable", async () => {
-      var line;
-      while (null !== (line = lineStream.read())) {
-        if (!header) {
-          header = line;
-        } else {
-          if (lineIndex === 0) {
-            if (outputStream) {
-              outputStream.end();
-            }
-            outputStream = await createOutputStreamCallback(chunkIndex++);
-            outputStream.write(header);
-            outputStream.write(options.delimiter);
+    lineStream.forEach(async (line) => {
+      let relLineIndex = lineIndex % options.lineLimit;
+      if (!header) {
+        header = line;
+      } else {
+        if (relLineIndex === 0) {
+          if (outputStream) {
+            outputStream.end();
           }
-
-          outputStream.write(line);
+          outputStream = await createOutputStreamCallback(chunkIndex++);
+          outputStream.write(header);
           outputStream.write(options.delimiter);
-          lineIndex = ++lineIndex % options.lineLimit;
         }
+
+        outputStream.write(line);
+        outputStream.write(options.delimiter);
+        lineIndex++;
       }
     });
 
     lineStream.on("error", (err) => handleError(err));
 
-    lineStream.on("end", () => {
+    lineStream.on("end", async () => {
       if (!header) {
         reject(new Error("The provided CSV is empty"));
         return;
@@ -67,7 +72,7 @@ function split(inputStream, opts, createOutputStreamCallback) {
       if (outputStream) {
         outputStream.end();
       } else {
-        outputStream = createOutputStreamCallback(chunkIndex++);
+        outputStream = await createOutputStreamCallback(chunkIndex++);
         outputStream.write(header);
         outputStream.write(options.delimiter);
         outputStream.end();
@@ -81,6 +86,91 @@ function split(inputStream, opts, createOutputStreamCallback) {
   });
 }
 
+/**
+ *
+ * @param {Readable} inputStream readable csv stream
+ * @param {any} opts delimeter and lineLimit options as an object
+ * @param {function} createOutputStringCallback callback function to be executed on each chunk.
+ * @returns {Promise<object>} returns object with total chunks and specified options
+ */
+function splitString(inputStream, opts, createOutputStringCallback) {
+  let outputString = "";
+  let chunkIndex = 0;
+  let lineIndex = 0;
+  let header;
+  const options = {
+    delimiter: opts.delimiter || "\n",
+    lineLimit: opts.lineLimit
+  };
+
+  return new Promise((resolve, reject) => {
+    assert(inputStream, "Provide inputStream");
+    assert(options.lineLimit > 0, "Provide non-negative lineLimit");
+    let lineStream;
+
+    function handleError(err) {
+      if (outputString) {
+        outputString.end();
+      }
+      reject(err);
+      return;
+    }
+
+    inputStream.on("error", handleError);
+
+    try {
+      lineStream = byline(inputStream);
+    } catch (err) {
+      handleError(err);
+      return;
+    }
+
+    lineStream.forEach(async (line) => {
+      let relLineIndex = lineIndex % options.lineLimit;
+      if (!header) {
+        header = line;
+      } else {
+        if (relLineIndex === 0) {
+          if (outputString) {
+            await createOutputStringCallback(outputString, chunkIndex++);
+            outputString = "";
+          }
+          outputString += header;
+          outputString += options.delimiter;
+        }
+
+        outputString += line;
+        outputString += options.delimiter;
+        lineIndex++;
+      }
+    });
+
+    lineStream.on("error", (err) => handleError(err));
+
+    lineStream.on("end", async () => {
+      if (!header) {
+        reject(new Error("The provided CSV is empty"));
+        return;
+      }
+
+      if (outputString) {
+        await createOutputStringCallback(outputString, chunkIndex++);
+        outputString = "";
+      } else {
+        outputString += header;
+        outputString += options.delimiter;
+        await createOutputStringCallback(outputString, chunkIndex++);
+      }
+
+      resolve({
+        totalChunks: chunkIndex,
+        options: options
+      });
+    });
+  });
+}
+
 module.exports = {
-  split
+  splitStream,
+  splitString
 };
